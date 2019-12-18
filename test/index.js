1909707efcaaf9ca9cfa53f638a1d63df4854680
eyzi <contact@eyzi.xyz>
2019-12-18 17:01:09 -0600

    added some tests

--- test/index.js
+++ test/index.js
@@ -6,77 +6,13 @@ const Destination = require("../src/structure/Destination");
 
 let manager = new Pixia();
 
-let s1 = manager.addStation({
-    name:"Test Station",
-    sources:[
-        {
-            data: "172.16.0.5/1",
-            label: ["live"]
-        },
-        {
-            data: "172.16.0.5/2"
-        },
-        {
-            data: "172.16.0.5/3"
-        },
-        {
-            data: "172.16.0.5/4"
-        }
-    ],
-    destinations:[
-        {
-            data: "172.16.0.5/1",
-            label: ["live"]
-        },
-        {
-            data: "172.16.0.5/2"
-        },
-        {
-            data: "172.16.0.5/3"
-        },
-        {
-            data: "172.16.0.5/4"
-        }
-    ],
-    gpis:[
-        {
-            data: "172.16.0.11/1"
-        }
-    ],
-    gpos:[
-        {
-            data: "172.16.0.11/1"
-        }
-    ]
+manager.on("destinations",list=>{
+    console.log(list.keys());
+    // console.log('Destinations: ',list.values().map(e=>e.toString()));
 });
-
-s1
-.on("source",data=>{
-    console.log('src',data.toString());
-})
-.on("destination",data=>{
-    console.log('dst',data.toString());
-})
-.on("gpi",data=>{
-    console.log('gpi',data.gpio.toString());
-})
-.on("gpo",data=>{
-    console.log('gpo',data.gpio.toString());
-})
-.on("subscribe",data=>{
-    console.log('subscribe',data.src.toString());
-})
-.on("unsubscribe",data=>{
-    console.log('unsubscribe',data.src.toString());
-})
-.on("meter",data=>{
-    //console.log(data);
-})
-
-// TODO
-.on("audio",data=>{
-    console.log('audio');
-})
-.on("silence",data=>{
-    console.log('silence');
+manager.on("sources",list=>{
+    console.log(list.keys());
+    // console.log('Sources: ',list.values().map(e=>e.toString()));
 });
+manager.addAddress("172.16.0.11");
+manager.addAddress("172.16.0.5");
